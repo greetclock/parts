a6149db38900afcbe7ad0e24a35a6d4a13b35a8c
Alexey Karpov <karpovad@yandex.ru>
2022-06-30 09:34:10 +0200

    initial commit

---
+++ apps/parts/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'parts-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'parts';
+}
