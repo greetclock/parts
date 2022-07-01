109d1092ce18c5a619aede701b3a1fa5ca818abc
Alexey Karpov <karpovad@yandex.ru>
2022-07-01 09:24:13 +0200

    test(parts): remove demo tests

--- apps/parts/src/app/app.component.spec.ts
+++ apps/parts/src/app/app.component.spec.ts
@@ -8,25 +8,4 @@ describe('AppComponent', () => {
       declarations: [AppComponent, NxWelcomeComponent],
     }).compileComponents();
   });
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'parts'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('parts');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome parts'
-    );
-  });
 });
