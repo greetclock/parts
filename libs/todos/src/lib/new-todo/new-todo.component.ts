70bc42efd3fb7c5d0d15ebeec44da970eae99cd1
Alexey Karpov <karpovad@yandex.ru>
2022-07-03 13:58:23 +0200

    feat(todos): closing new-todo on clickout

--- libs/todos/src/lib/new-todo/new-todo.component.ts
+++ libs/todos/src/lib/new-todo/new-todo.component.ts
@@ -1,7 +1,13 @@
-import { Component, OnDestroy } from '@angular/core'
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  OnDestroy,
+  OnInit,
+} from '@angular/core'
 import { TodosFacadeService } from '@parts/todos/data'
 import { RxState } from '@rx-angular/state'
-import { Subject, takeUntil } from 'rxjs'
+import { mergeMap, partition, skip, Subject, takeUntil, tap } from 'rxjs'
 import { TodosMainComponentState } from '../todos-main/todos-main.component'
 
 @Component({
@@ -9,35 +15,80 @@ import { TodosMainComponentState } from '../todos-main/todos-main.component'
   templateUrl: './new-todo.component.html',
   styleUrls: ['./new-todo.component.css'],
 })
-export class NewTodoComponent implements OnDestroy {
+export class NewTodoComponent implements OnInit, OnDestroy {
   title = ''
   description = ''
 
   private destroy$ = new Subject<void>()
 
+  private outsideClicks$ = new Subject<void>()
+
   constructor(
     private todosFacade: TodosFacadeService,
-    private state: RxState<TodosMainComponentState>
+    private state: RxState<TodosMainComponentState>,
+    private elementRef: ElementRef
   ) {}
 
   save() {
-    this.todosFacade
-      .createTodo({
-        title: this.title,
-        description: this.description,
-      })
-      .pipe(takeUntil(this.destroy$))
-      .subscribe(() => this.disableAddingNew())
+    this.save$().subscribe()
+  }
+
+  @HostListener('document:click', ['$event'])
+  clickOutside(event: Event) {
+    if (!event) {
+      return
+    }
+
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      this.outsideClicks$.next()
+    }
+  }
+
+  ngOnInit(): void {
+    this.watchOutsideClicks()
   }
 
   ngOnDestroy(): void {
     this.destroy$.next()
     this.destroy$.complete()
+
+    this.outsideClicks$.complete()
   }
 
   private disableAddingNew() {
     this.state.set({
       addingNew: false,
     })
+  }
+
+  private watchOutsideClicks() {
+    const clicks$ = this.closingClicks$()
+    const [withData$, withoutData$] = partition(clicks$, () =>
+      this.userEnteredData()
+    )
+
+    withoutData$.subscribe(() => this.disableAddingNew())
+
+    withData$.pipe(mergeMap(() => this.save$())).subscribe()
+  }
+
+  private userEnteredData(): boolean {
+    return this.title !== '' || this.description !== ''
+  }
+
+  private save$() {
+    return this.todosFacade
+      .createTodo({
+        title: this.title,
+        description: this.description,
+      })
+      .pipe(
+        tap(() => this.disableAddingNew()),
+        takeUntil(this.destroy$)
+      )
+  }
+
+  private closingClicks$() {
+    return this.outsideClicks$.pipe(skip(1), takeUntil(this.destroy$))
   }
 }
