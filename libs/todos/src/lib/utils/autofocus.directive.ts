d2fd657b8cc721a6a87489a54c8cb7ac87e9943f
Alexey Karpov <karpovad@yandex.ru>
2022-07-09 22:29:24 +0200

    feat(todos): make sure NOT to autofocus on input when user opens existing todo element

--- libs/todos/src/lib/utils/autofocus.directive.ts
+++ libs/todos/src/lib/utils/autofocus.directive.ts
@@ -1,12 +1,20 @@
-import { AfterViewInit, Directive, ElementRef } from '@angular/core'
+import { AfterViewInit, Directive, ElementRef, Input } from '@angular/core'
 
 @Directive({
   selector: '[partsAutofocus]',
 })
 export class AutofocusDirective implements AfterViewInit {
+  private enableAutofocus = true
+
+  @Input() set partsAutofocus(enableAutofocus: boolean) {
+    this.enableAutofocus = enableAutofocus
+  }
+
   constructor(private host: ElementRef) {}
 
   ngAfterViewInit() {
-    this.host.nativeElement.focus()
+    if (this.enableAutofocus) {
+      this.host.nativeElement.focus()
+    }
   }
 }
