8e30d12fa6d106695471c148343a5df94864a064
Alexey Karpov <karpovad@yandex.ru>
2022-07-10 10:57:28 +0200

    test(todos): added tests for sidebar

--- libs/todos/src/lib/components/sidebar/sidebar.component.spec.ts
+++ libs/todos/src/lib/components/sidebar/sidebar.component.spec.ts
@@ -1,14 +1,44 @@
-import { Spectator, createComponentFactory } from '@ngneat/spectator/jest'
-
+import { fakeAsync, tick } from '@angular/core/testing'
+import {
+  createComponentFactory,
+  mockProvider,
+  Spectator,
+} from '@ngneat/spectator/jest'
+import { TodosFacadeService } from '@parts/todos/data'
+import { defer, of } from 'rxjs'
 import { SidebarComponent } from './sidebar.component'
 
 describe('SidebarComponent', () => {
+  let todosNumber: number
+
   let spectator: Spectator<SidebarComponent>
-  const createComponent = createComponentFactory(SidebarComponent)
+  const createComponent = createComponentFactory({
+    component: SidebarComponent,
+    providers: [
+      mockProvider(TodosFacadeService, {
+        todosNumber$: defer(() => of(todosNumber)),
+      }),
+    ],
+  })
+
+  beforeEach(() => {
+    todosNumber = 0
+  })
 
   it('should create', () => {
     spectator = createComponent()
 
     expect(spectator.component).toBeTruthy()
   })
+
+  it('should watch the number of todos', fakeAsync(() => {
+    todosNumber = 10
+
+    spectator = createComponent()
+    tick()
+
+    const todayLink = spectator.component.links.find((it) => it.id === 'today')
+
+    expect(todayLink?.endText).toEqual('10')
+  }))
 })
