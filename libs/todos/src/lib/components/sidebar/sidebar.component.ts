8c5d2a2ad40c48485f90543913eb96ab7ceadbad
Alexey Karpov <karpovad@yandex.ru>
2022-07-10 10:49:02 +0200

    feat(todos): showing the number of active todos

--- libs/todos/src/lib/components/sidebar/sidebar.component.ts
+++ libs/todos/src/lib/components/sidebar/sidebar.component.ts
@@ -1,30 +1,71 @@
-import { Component, EventEmitter, Output } from '@angular/core'
+import {
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core'
+import { TodosFacadeService } from '@parts/todos/data'
+import { Subject, takeUntil } from 'rxjs'
 
 interface Link {
+  id: string
   link: string
   text: string
+  endText?: string
 }
+
+const TODAY_ID = 'today'
 
 @Component({
   selector: 'parts-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit, OnDestroy {
   @Output() linkSelected = new EventEmitter<Link>()
 
   links: Link[] = [
     {
+      id: TODAY_ID,
       link: '/todos/today',
       text: '⭐️ To-Do List',
+      endText: '',
     },
     {
+      id: 'logbook',
       link: '/todos/logbook',
       text: '✅ Logbook',
     },
   ]
 
+  private destroy$ = new Subject<void>()
+
+  constructor(protected todosFacade: TodosFacadeService) {}
+
+  ngOnInit(): void {
+    this.watchTodosNumber()
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next()
+    this.destroy$.complete()
+  }
+
   onLinkSelected(link: Link) {
     this.linkSelected.next(link)
+  }
+
+  private watchTodosNumber() {
+    this.todosFacade.todosNumber$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((todosNumber) => {
+        const todosLink = this.links.find((link) => link.id === TODAY_ID)
+        if (!todosLink) {
+          throw new Error(`Link was not found!`)
+        }
+
+        todosLink.endText = '' + todosNumber
+      })
   }
 }
