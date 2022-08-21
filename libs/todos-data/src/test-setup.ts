431bec2d30114a1769f58ec80a76ef0f29cb698a
Alexey Karpov <karpovad@yandex.ru>
2022-08-21 14:50:56 +0200

    test(todos-data): fix test

--- libs/todos-data/src/test-setup.ts
+++ libs/todos-data/src/test-setup.ts
@@ -1,1 +1,8 @@
 import 'jest-preset-angular/setup-jest'
+import * as crypto from 'crypto'
+
+Object.defineProperty(global.self, 'crypto', {
+  value: {
+    getRandomValues: (arr: any) => crypto.randomBytes(arr.length),
+  },
+})
